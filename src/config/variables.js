d8c5971e45919934f743a3b50b8f490057a3eba5
Jhon Angulo <jhon.angulo@jikkosoft.com>
2022-01-10 17:57:20 -0500

    dinamic port

--- src/config/variables.js
+++ src/config/variables.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 module.exports = {
   system: {
     production: process.env.NODE_ENV === 'production',
-    port: process.env.PORT,
+    port: process.env.PORT || 3000,
     allow_origin: JSON.parse(process.env.ALLOW_ORIGIN)
   },
   db: {
